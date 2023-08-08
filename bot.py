121790cefcf9b7365c3d0e2427847c6b6d5279c1
VegaNicolas <nicolasgarciavega17@gmail.com>
2023-08-08 17:27:51 -0300

    deprecated update_dates() method

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ import tweepy
 load_dotenv()
 
 # APIs' setup
-## Telegram
+# Telegram
 TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
 
-## Twitter
+# Twitter
 API_KEY_TW: str = os.getenv("API_KEY_TW")
 API_KEY_SECRET_TW: str = os.getenv("API_KEY_SECRET_TW")
 BEARER_TOKEN_TW: str = os.getenv("BEARER_TOKEN_TW")
@@ -23,44 +23,39 @@ ACCESS_TOKEN_TW: str = os.getenv("ACCESS_TOKEN_TW")
 ACCESS_TOKEN_SECRET_TW: str = os.getenv("ACCESS_TOKEN_SECRET_TW")
 
 
-
 # Global Variables
 intervalo_blue: float = 0  # Precio del intervalo anterior
 apertura_blue: float = 0
 start: str = datetime.now().replace(hour=8, minute=55, second=0)
 
+
 class Bot():
 
     def __init__(self):
         # Telegram
-        self.channel_id:str = os.getenv("CHANNEL_ID")
-        self.telegram_client:telebot.TeleBot = telebot.TeleBot(TELEGRAM_TOKEN)
+        self.channel_id: str = os.getenv("CHANNEL_ID")
+        self.telegram_client: telebot.TeleBot = telebot.TeleBot(TELEGRAM_TOKEN)
 
         # Twitter
         self.twitter_client: tweepy.Client = tweepy.Client(
-                                consumer_key=API_KEY_TW,
-                                consumer_secret=API_KEY_SECRET_TW,
-                                bearer_token=BEARER_TOKEN_TW,
-                                access_token=ACCESS_TOKEN_TW,
-                                access_token_secret=ACCESS_TOKEN_SECRET_TW
-                                )
-
-
+            consumer_key=API_KEY_TW,
+            consumer_secret=API_KEY_SECRET_TW,
+            bearer_token=BEARER_TOKEN_TW,
+            access_token=ACCESS_TOKEN_TW,
+            access_token_secret=ACCESS_TOKEN_SECRET_TW
+        )
 
     def update_json(self):
-
         """ Takes an URL as a parameter (unable due to schedule library #TODO, so hardcoded) which leads
             to a json in Ámbito Financiero webpage (Argentinian news media that follows the informal ARS/USD rate)
             which is the source of the information used in every bot done by me."""
-            
+
         URL: str = os.getenv("URL")
         result: str = requests.get(URL).text
         datos: list = json.loads(result)
         return datos
 
-
     def startup(self):
-
         """ QoL function which is called only when the service is restarted. It allows better
             testing results and even to commit changes in production on a weekday. The function settles
             the two main variables in the script (apertura_blue and intervalo_blue) which are not saved
@@ -70,60 +65,50 @@ class Bot():
         global apertura_blue
         global intervalo_blue
         data: list = self.update_json()
-        apertura_blue = float(data["valor_cierre_ant"].replace(",","."))
-        intervalo_blue = float(data["venta"].replace(",","."))
+        apertura_blue = float(data["valor_cierre_ant"].replace(",", "."))
+        intervalo_blue = float(data["venta"].replace(",", "."))
 
         now: str = datetime.now().strftime("%d/%m/%y - %H:%M:%S")
         print(f"{now} - Arranca la jornada. Dólar Blue abrió en ${int(apertura_blue)}.")
 
-
-    def non_business_day(self):
-
+    '''def non_business_day(self):
         """ This function compares actual datetime with the one it is in the json file (updated rate)
             so the bot gives zero output during weekends and non-business days.
             *Note: this function may be deprecated in the future due to the json doesnt update in those days.
             It only affects cierre().
+            # TODO Legacy method, deprecated in 8/8 due to api changes
         """
 
         data: list = self.update_json()
         fecha_feriado: str = data["fecha"]
         dia_update: list[str] = fecha_feriado.split(" ")
 
-        return dia_update[0] != datetime.now().strftime("%d/%m/%Y")
+        return dia_update[0] != datetime.now().strftime("%d/%m/%Y")'''
 
-
-    def opening(self): #11.05hs
-
+    def opening(self):  # 11.05hs
         """ Internal function which updates the apertura_blue variable if been outaged during night.
         """
 
         global apertura_blue
 
-        if self.non_business_day():
-            return
-        
         data: list = self.update_json()
         now_open: str = datetime.now().strftime("%d/%m/%y - %H:%M:%S")
-        apertura_blue = float(data["valor_cierre_ant"].replace(",","."))
+        apertura_blue = float(data["valor_cierre_ant"].replace(",", "."))
         print(f"{now_open} - El dólar abre a ${apertura_blue}.")
 
-    
-    
-    def update_dates(self): #19.01hs
-
+    def update_dates(self):  # 19.01hs
         """ This updates the start variable at 19.01 (GMT-3) and sums 24hours in order to resume bot's functionality the next day 
             at 8.55 (GMT-3). In the future, this variable may be modified to 11.00 (GMT-3) to be more optimized.
         """
 
         global start
+
         start += timedelta(days=1)
         now_change: str = datetime.now().strftime("%d/%m/%y - %H:%M:%S")
-        print(f"------------------------------------\n{now_change} - Fechas actualizadas\n------------------------------------")
+        print(
+            f"{'-'*36}\n{now_change} - Fechas actualizadas\n{'-'*36}")
 
-
-
-    def core(self): 
-        
+    def core(self):
         """ Main output function. Every 5 minutes will update_json() so if it detects a new rate, 
             it will be the output of the bot. It isn't affected by the function arranque() because
             its only triggered when a change is made.
@@ -131,61 +116,61 @@ class Bot():
 
         global intervalo_blue
 
-        if datetime.now().strftime("%H:%M:%S") == "16:30:00": 
+        if datetime.now().strftime("%H:%M:%S") == "16:30:00":
             return
-        
-        if self.non_business_day():
-            return
-        
 
-        data: list = self.update_json() # TODO maybe this function is called twice because of non_business_day()
+        # TODO maybe this function is called twice because of non_business_day() * no anymore
+        data: list = self.update_json()
 
-        venta_blue: float = float(data["venta"].replace(",","."))
+        venta_blue: float = float(data["venta"].replace(",", "."))
         now_change: str = datetime.now().strftime("%d/%m/%y - %H:%M:%S")
 
         if intervalo_blue > venta_blue:
-            self.telegram_client.send_message(chat_id=self.channel_id, text=f"El Dólar Blue bajó ${int(intervalo_blue - venta_blue)} a ${int(venta_blue)}.")
-            self.twitter_client.create_tweet(text=f"El Dólar Blue bajó ${int(intervalo_blue - venta_blue)} a ${int(venta_blue)}.") 
+            self.telegram_client.send_message(
+                chat_id=self.channel_id, text=f"El Dólar Blue bajó ${int(intervalo_blue - venta_blue)} a ${int(venta_blue)}.")
+            self.twitter_client.create_tweet(
+                text=f"El Dólar Blue bajó ${int(intervalo_blue - venta_blue)} a ${int(venta_blue)}.")
             intervalo_blue = venta_blue
             print(f"{now_change} - Went down to ${int(intervalo_blue)} ARS/USD")
-            
+
         elif intervalo_blue < venta_blue:
-            self.telegram_client.send_message(chat_id=self.channel_id, text=f"El Dólar Blue subió ${int(venta_blue - intervalo_blue)} a ${int(venta_blue)}.")
-            self.twitter_client.create_tweet(text=f"El Dólar Blue subió ${int(venta_blue - intervalo_blue)} a ${int(venta_blue)}.")
+            self.telegram_client.send_message(
+                chat_id=self.channel_id, text=f"El Dólar Blue subió ${int(venta_blue - intervalo_blue)} a ${int(venta_blue)}.")
+            self.twitter_client.create_tweet(
+                text=f"El Dólar Blue subió ${int(venta_blue - intervalo_blue)} a ${int(venta_blue)}.")
             intervalo_blue = venta_blue
             print(f"{now_change} - Raised to ${int(intervalo_blue)} ARS/USD")
-        
 
-    def closing(self): # 16.30hs
-
+    def closing(self):  # 16.30hs
         """ Output a closing rate exchange message at 16.30 (GMT-3), although the function core() is still running until
             19.00 (GMT-3), so sometimes cierre() might not be the last output of the day.
         """
 
         global apertura_blue
-        if self.non_business_day():
-            now_open: str = datetime.now().strftime("%d/%m/%y - %H:%M:%S")
-            print(f"{now_open} - Non-business day, so no activity")
-            return
-        
+
         data: list = self.update_json()
         variation: str = data["variacion"]
         now_open: str = datetime.now().strftime("%d/%m/%y - %H:%M:%S")
-        cierre_blue: float = float(data["venta"].replace(",","."))
+        cierre_blue: float = float(data["venta"].replace(",", "."))
 
         if cierre_blue > apertura_blue:
-            self.telegram_client.send_message(chat_id=self.channel_id, text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día subió ${int(cierre_blue - apertura_blue)}, lo que significa una variación de {variation}.")
-            self.twitter_client.create_tweet(text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día subió ${int(cierre_blue - apertura_blue)}, lo que significa una variación de {variation}.")
+            self.telegram_client.send_message(
+                chat_id=self.channel_id, text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día subió ${int(cierre_blue - apertura_blue)}, lo que significa una variación de {variation}.")
+            self.twitter_client.create_tweet(
+                text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día subió ${int(cierre_blue - apertura_blue)}, lo que significa una variación de {variation}.")
             print(f"{now_open} - Closed at {cierre_blue} ARS/USD")
 
         elif cierre_blue < apertura_blue:
-            self.telegram_client.send_message(chat_id=self.channel_id, text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día bajó ${int(apertura_blue - cierre_blue)}, lo que significa una variación de {variation}.")
-            self.twitter_client.create_tweet(text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día bajó ${int(apertura_blue - cierre_blue)}, lo que significa una variación de {variation}.")
-            
+            self.telegram_client.send_message(
+                chat_id=self.channel_id, text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día bajó ${int(apertura_blue - cierre_blue)}, lo que significa una variación de {variation}.")
+            self.twitter_client.create_tweet(
+                text=f"El Dólar Blue cierra la jornada a ${int(cierre_blue)}. En el día bajó ${int(apertura_blue - cierre_blue)}, lo que significa una variación de {variation}.")
+
             print(f"{now_open} - Closed at {cierre_blue} ARS/USD")
 
         else:
-            self.telegram_client.send_message(chat_id=self.channel_id, text=f"El Dólar Blue cierra la jornada sin cambios a ${int(cierre_blue)}.")
-            self.twitter_client.create_tweet(text=f"El Dólar Blue cierra la jornada sin cambios a ${int(cierre_blue)}.")
+            self.telegram_client.send_message(
+                chat_id=self.channel_id, text=f"El Dólar Blue cierra la jornada sin cambios a ${int(cierre_blue)}.")
+            self.twitter_client.create_tweet(
+                text=f"El Dólar Blue cierra la jornada sin cambios a ${int(cierre_blue)}.")
             print(f"{now_open} - Closed at {int(cierre_blue)} ARS/USD")
-
--- main.py
+++ main.py
@@ -12,21 +12,20 @@ if __name__ == "__main__":
 
     client = bot.Bot()
 
-     # Schedule Setup
+    # Schedule Setup
     client.startup()
 
-    minutes = ["00","05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]
+    minutes = ["00", "05", "10", "15", "20",
+               "25", "30", "35", "40", "45", "50", "55"]
 
     for i in minutes:
         schedule.every().hour.at(f":{i}").do(client.core)
-    
-    
+
     schedule.every().day.at("11:05:00").do(client.opening)
     schedule.every().day.at("16:30:00").do(client.closing)
     schedule.every().day.at("19:01:00").do(client.update_dates)
-    
-    
-    while True:      
+
+    while True:
         if bot.start < datetime.now():
             schedule.run_pending()
         else:
