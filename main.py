2ec5f0faea53725c93d6f43d188254b654ef0bcf
VegaNicolas <nicolasgarciavega17@gmail.com>
2024-02-10 07:14:47 -0300

    Add Bluesky client to schedule

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ from Bot.update_dates import update_dates, start
 from Database.database import update_database, update_price
 from SocialMedia.telegram import telegram_client
 from SocialMedia.twitter import twitter_client
+from SocialMedia.bluesky import bluesky_client
 
 
 def main():
-    print("Bot Ready!")
-
     # Schedule Setup
     startup()
     update_database()
-    print('Database Updated')
+    print('Database Updated \nBot Ready!')
 
     minutes = ["00", "05", "10", "15", "20",
                "25", "30", "35", "40", "45", "50", "55"]
@@ -22,15 +21,17 @@ def main():
     schedule.every().day.at("16:30:00").do(
         telegram_client.closing)  # 16.30 -> 19.30 UTC
     schedule.every().day.at("16:30:00").do(twitter_client.closing)
+    schedule.every().day.at("16:30:00").do(bluesky_client.closing)
 
     for i in minutes:
         schedule.every().hour.at(f":{i}").do(update_price)
         schedule.every().hour.at(f":{i}").do(telegram_client.core)
+        schedule.every().hour.at(f":{i}").do(bluesky_client.core)
         schedule.every().hour.at(f":{i}").do(twitter_client.core)
         schedule.every().hour.at(f":{i}").do(update_database)
 
     schedule.every().day.at("11:05:00").do(startup)  # 11.05 -> 14.05 UTC
-    schedule.every().day.at("21:01:00").do(update_dates)  # 19.01 -> 22.01 UTC
+    schedule.every().day.at("19:01:00").do(update_dates)  # 19.01 -> 22.01 UTC
 
     while True:
         if start < datetime.now():
