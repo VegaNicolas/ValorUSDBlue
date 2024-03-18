91cf313458e4cda5e6bd53061209be83eef200d7
VegaNicolas <nicolasgarciavega17@gmail.com>
2024-03-18 14:53:02 -0300

    Change link to get actual date

--- Bot/non_business_day.py
+++ Bot/non_business_day.py
@@ -1,4 +1,4 @@
-from Bot.update_json import update_json
+from Bot.update_json import update_ccl
 from datetime import datetime
 
 
@@ -7,7 +7,7 @@ def non_business_day() -> bool:
         so the bot gives zero output during weekends and non-business days.
         *Note: this function may be deprecated in the future due to the json doesnt update in those days."""
 
-    data: list = update_json()
+    data: list = update_ccl()
     fecha_feriado: str = data["fecha"]
     dia_update: list[str] = fecha_feriado.split(" ")
 
